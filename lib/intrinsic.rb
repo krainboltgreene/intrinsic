b6bfc6f9792cef739a2f3b6f3f60679a34a4e529
krainboltgreene <me@kurtisrainboltgreene.name>
2012-01-08 01:56:54 -0600

    On initialize check the properties of list and define private method

--- lib/intrinsic.rb
+++ lib/intrinsic.rb
@@ -11,10 +11,14 @@ module Intrinsic
 
   def initialize(values = {})
     @properties = {}.merge self.class.defaults
+    check_properties_of values
   end
 
   def to_s
     "#<#{self.class}:#{self.object_id} #{properties.map{|k,v| k.to_s + '=' + v.inspect}.join(', ')}>"
   end
 
+  private
+    def check_properties_of(values)
+    end
 end
