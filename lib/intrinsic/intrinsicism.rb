35d3aa4b95bcd2df931b89ccf19c62374c08acad
krainboltgreene <me@kurtisrainboltgreene.name>
2012-01-08 01:50:06 -0600

    Method for checking the types of the arguments

--- lib/intrinsic/intrinsicism.rb
+++ lib/intrinsic/intrinsicism.rb
@@ -6,7 +6,13 @@ module Intrinsic
     def property(name, type = String, options = {})
       @properties ||= []
       @defaults ||= {}
+      check_types_for name, type, options
     end
 
+      def check_types_for(name, type, options)
+        raise TypeError unless name.is_a? Symbol
+        raise TypeError unless Intrinsic.const_get(type.name.to_sym).is_a? Class
+        raise TypeError unless options.is_a? Hash
+      end
   end
 end
