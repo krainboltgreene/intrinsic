7e3b172399902b543ef5a74636c7cbeb2c2d3644
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-01-09 22:37:23 -0600

    Wrapping coercion in begin/rescue blocks with raise typerrors

--- lib/intrinsic/intrinsicism/coercion/integer.rb
+++ lib/intrinsic/intrinsicism/coercion/integer.rb
@@ -1,9 +1,13 @@
-      value.to_i
 module Intrinsic
   module Intrinsicism
     module Coercion
       module Integer
         def self.convert(value)
+          begin
+            value.to_i
+          rescue
+            raise TypeError
+          end
         end
       end
     end
--- lib/intrinsic/intrinsicism/coercion/string.rb
+++ lib/intrinsic/intrinsicism/coercion/string.rb
@@ -1,9 +1,13 @@
-      value.to_s
 module Intrinsic
   module Intrinsicism
     module Coercion
       module String
         def self.convert(value)
+          begin
+            value.to_s
+          rescue
+            raise TypeError
+          end
         end
       end
     end
