610e5d3412e27fad68346cec2f89398c48fee26a
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-01-09 22:37:47 -0600

    Adding the hash coercion logic

--- lib/intrinsic/intrinsicism/coercion.rb
+++ lib/intrinsic/intrinsicism/coercion.rb
@@ -1,6 +1,7 @@
 require_relative 'coercion/string'
 require_relative 'coercion/integer'
 require_relative 'coercion/array'
+require_relative 'coercion/hash'
 require_relative 'coercion/proc'
 
 module Intrinsic::Intrinsicism
